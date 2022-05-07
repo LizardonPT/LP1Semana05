86c7b7658c8f467c9c3889b298546d3aa9995aff
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-05-07 14:00:49 +0100

    Chronos exercice

---
+++ Chronos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch cron1 = new Stopwatch();
+            Stopwatch cron2 = new Stopwatch();
+
+            cron1.Start();
+            Thread.Sleep(1000);
+            cron2.Start();
+            Thread.Sleep(1000);
+            cron1.Stop();
+            cron2.Stop();
+            
+            TimeSpan tcron1 = cron1.Elapsed;
+            TimeSpan tcron2 = cron2.Elapsed;
+            Console.WriteLine($"{tcron1} sec");
+            Console.WriteLine($"{tcron2} sec");
+        }
+    }
+}
