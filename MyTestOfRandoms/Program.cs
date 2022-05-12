0338890f1d22a5041899860729a57eacc919dc32
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-05-12 13:27:35 +0100

    MyTestOfRandoms

---
+++ MyTestOfRandoms/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyTestOfRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+            Console.Write("Número de dados a lançar: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int dsoma = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                int d = random.Next(1,7);
+                Console.WriteLine(d);
+                dsoma += d;
+            }
+            Console.Write("Soma: ");
+            Console.Write(dsoma);
+        }
+    }
+}
